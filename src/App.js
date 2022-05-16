43e638455bdee51f8ca67569348681a3d2e62665
Abdullah Al Tabbal <xlp1abode@gmail.com>
2022-05-16 20:38:39 +0200

    build structure with styled component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
+import { useEffect, useState } from "react";
+
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1em;
+  border-bottom: 2px dotted white;
+  background-color: hsl(226, 41%, 27%);
+  color: white;
+
+  h1 {
+    letter-spacing: 10px;
+  }
+`;
+
+const Main = styled.div`
+  flex: 1;
+`;
+
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/solution")
+      .then((res) => res.json())
+      .then((json) => {
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setSolution(randomSolution.word);
+      });
+  }, [setSolution]);
   return (
-    <div>
-      hi
-    </div>
+    <Wrapper>
+      <Header>
+        <h1>Wordle</h1>
+      </Header>
+      <Main></Main>
+    </Wrapper>
   );
 }
 
